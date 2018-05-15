49765e081256d25a765f444ae2a7e484932d3f38
Jakub Prüher <jacobnzw@gmail.com>
2018-05-15 15:59:34 +0200

    Barebones project structure outlined.

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+CALIB_DIR = 'camera_cal'
+OUT_DIR = 'output_images'
+TEST_DIR = 'test_images'
+
+# TODO: camera calibration
+# TODO: perspective transformation
+# TODO: HSV, HSL, gradient thresholding
+# TODO: lane pixel identification
+# TODO: fit curve through the lane pixels
+# TODO: compute curvature
+# TODO: inverse perspective transformation
+# TODO: mark lanes graphically
