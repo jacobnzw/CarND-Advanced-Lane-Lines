85c04ff922a02e3652239899cb06df84ddb70e61
Jakub Prüher <jacobnzw@gmail.com>
2018-05-17 16:20:02 +0200

    Undistortion demo code put into fuction as a test for future debugging. Notes added: figured out how to produce the perspective transformation matrices.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def distortion_coefficients():
     img_points = []  # 2D points in an image
 
     # create 3D object points, where the last dimension is always set to 0
-    objp = np.zeros(np.prod(CHESSBOARD_PATTERN_SIZE), 3)
+    objp = np.zeros((np.prod(CHESSBOARD_PATTERN_SIZE), 3), np.float32)
     objp[:, :2] = np.mgrid[:CHESSBOARD_PATTERN_SIZE[0], :CHESSBOARD_PATTERN_SIZE[1]].T.reshape(-1, 2)
 
     for image_filename in dir_list:
@@ -44,17 +44,30 @@ def distortion_coefficients():
     # calculate camera matrix and distortion coefficient
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img.shape[::-1], None, None)
 
-    return mtx, dist
+    return mtx, dist, corners
+
+
+def undistortion_test():
+    # compute distortion coefficients and the camera matrix
+    camera_mat, dist_coeff, _ = distortion_coefficients()
+    # read in a calibration image
+    dir_list = os.listdir(CALIB_DIR)
+    img = cv2.imread(os.path.join(CALIB_DIR, dir_list[11]))
+    undistorted = cv2.undistort(img, camera_mat, dist_coeff, None, camera_mat)
+    # show the difference
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax[1].imshow(cv2.cvtColor(undistorted, cv2.COLOR_BGR2RGB))
+    plt.show()
 
 
 # correct for camera lens distortion
-camera_mat, dist_coeff = distortion_coefficients()
-undistorted = cv2.undistort(img, camera_mat, dist_coeff, None, camera_mat)
 
 # TODO: perspective transformation
-# source points and destination points
-src = []
-dst = []
+# source points and destination points must match each other; src[0] --> dst[0]
+# each point has coordinates [x, y], where img.shape = (y, x)
+src = []  # points in the image (e.g. corners of chessboard, vertices of a rectangle marking the lane etc.)
+dst = []  # points with desired coordinates in the destination image
 trans_mat = cv2.getPerspectiveTransform(src, dst)
 trans_mat_inverse = cv2.getPerspectiveTransform(dst, src)
 warped = cv2.warpPerspective(src, trans_mat, img.shape[::-1], flags=cv2.INTER_LINEAR)
