34ba96d94ce433b24c85498d24befdf1b5b2f040
Jakub Prüher <jacobnzw@gmail.com>
2018-05-22 19:10:43 +0200

    Thresholding turned into a function. Test function provided for experiments.

--- main.py
+++ main.py
@@ -10,6 +10,59 @@ TEST_DIR = 'test_images'
 CHESSBOARD_PATTERN_SIZE = (9, 6)
 
 
+def undistortion_test():
+    # compute distortion coefficients and the camera matrix
+    camera_mat, dist_coeff = distortion_coefficients()
+    # read in a calibration image
+    dir_list = os.listdir(CALIB_DIR)
+    img = cv2.imread(os.path.join(CALIB_DIR, dir_list[11]))
+    undistorted = cv2.undistort(img, camera_mat, dist_coeff, None, camera_mat)
+    # show the difference
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax[1].imshow(cv2.cvtColor(undistorted, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+
+def perspective_transform_test():
+    # points in the image (e.g. corners of chessboard, vertices of a rectangle marking the lane etc.)
+    src = np.array([[305, 650], [1000, 650], [685, 450], [595, 450]], np.float32)
+    # points with desired coordinates in the destination image
+    dst = np.array([[305, img.shape[0]], [1000, img.shape[0]], [1000, 0], [305, 0]], np.float32)
+    trans_mat = cv2.getPerspectiveTransform(src, dst)
+    # trans_mat_inverse = cv2.getPerspectiveTransform(dst, src)
+    cv2.polylines(img, [src.astype(np.int32)], True, [0, 0, 255], thickness=2)
+    warped = cv2.warpPerspective(img, trans_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+
+    cv2.polylines(img, [src.astype(np.int32)], True, [0, 0, 255], thickness=2)
+    cv2.polylines(warped, [dst.astype(np.int32)], True, [0, 255, 0], thickness=4)
+
+    fig, ax = plt.subplots(2, 1)
+    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax[1].imshow(cv2.cvtColor(warped, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+
+def thresholding_test():
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # compute gradients in x and y directions
+    sobel_x = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    sobel_y = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+    # scale to 0 - 255 and unsigned 8-bit integer type
+    sobel_x = np.uint8(255 * sobel_x / np.max(sobel_x))
+    sobel_y = np.uint8(255 * sobel_y / np.max(sobel_y))
+
+    sobel_x = np.logical_and(sobel_x >= 100, sobel_x <= 255)
+    sobel_y = np.logical_and(sobel_y >= 100, sobel_y <= 255)
+
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_thresh = np.logical_or(sobel_x, sobel_y)
+    img_thresh = np.logical_or(img_thresh, img_hsv[..., 2] > 245)
+
+    plt.imshow(img_thresh, cmap='gray')
+    plt.show()
+
+
 def distortion_coefficients():
     """
     Find distortion coefficients and camera matrix for `undistort()` function from calibration images.
@@ -47,40 +100,31 @@ def distortion_coefficients():
     return mtx, dist
 
 
-def undistortion_test():
-    # compute distortion coefficients and the camera matrix
-    camera_mat, dist_coeff, _ = distortion_coefficients()
-    # read in a calibration image
-    dir_list = os.listdir(CALIB_DIR)
-    img = cv2.imread(os.path.join(CALIB_DIR, dir_list[11]))
-    undistorted = cv2.undistort(img, camera_mat, dist_coeff, None, camera_mat)
-    # show the difference
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    ax[1].imshow(cv2.cvtColor(undistorted, cv2.COLOR_BGR2RGB))
-    plt.show()
+def thresholding(img_bgr):
+    # convert to grayscale
+    gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
+    # compute gradients in x and y directions
+    sobel_x = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    sobel_y = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+    # scale to 0 - 255 and unsigned 8-bit integer type
+    sobel_x = np.uint8(255 * sobel_x / np.max(sobel_x))
+    sobel_y = np.uint8(255 * sobel_y / np.max(sobel_y))
 
-def perspective_transform_test():
-    # points in the image (e.g. corners of chessboard, vertices of a rectangle marking the lane etc.)
-    src = np.array([[305, 650], [1000, 650], [685, 450], [595, 450]], np.float32)
-    # points with desired coordinates in the destination image
-    dst = np.array([[305, img.shape[0]], [1000, img.shape[0]], [1000, 0], [305, 0]], np.float32)
-    trans_mat = cv2.getPerspectiveTransform(src, dst)
-    # trans_mat_inverse = cv2.getPerspectiveTransform(dst, src)
-    cv2.polylines(img, [src.astype(np.int32)], True, [0, 0, 255], thickness=2)
-    warped = cv2.warpPerspective(img, trans_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+    # threshold in gradient space
+    sobel_x = np.logical_and(sobel_x >= 100, sobel_x <= 255)
+    sobel_y = np.logical_and(sobel_y >= 100, sobel_y <= 255)
 
-    cv2.polylines(img, [src.astype(np.int32)], True, [0, 0, 255], thickness=2)
-    cv2.polylines(warped, [dst.astype(np.int32)], True, [0, 255, 0], thickness=4)
+    # threshold in HSV space
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+    img_hsv = img_hsv[..., 2] > 245
 
-    fig, ax = plt.subplots(2, 1)
-    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    ax[1].imshow(cv2.cvtColor(warped, cv2.COLOR_BGR2RGB))
-    plt.show()
+    # combine thresholds
+    img_thresh = np.logical_or(sobel_x, sobel_y)
+    img_thresh = np.logical_or(img_thresh, img_hsv)
 
+    return img_thresh
 
-# region_of_interest_test()
 
 # load an image
 dir_list = os.listdir(TEST_DIR)
@@ -90,17 +134,10 @@ img = cv2.imread(os.path.join(TEST_DIR, dir_list[1]))
 camera_mat, dist_coeff = distortion_coefficients()
 img = cv2.undistort(img, camera_mat, dist_coeff, None, camera_mat)
 
-# TODO: make function for HSV, HSL, gradient thresholding
+img = thresholding(img)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# compute gradients in x and y directions
-sobel_y = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
-sobel_x = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
-# scale to 0 - 255 and unsigned 8-bit integer type
-sobel_y = np.uint8(255 * sobel_y / np.max(sobel_y))
-sobel_x = np.uint8(255 * sobel_x / np.max(sobel_x))
-plt.imshow(np.logical_and(sobel_y >= 100, sobel_y <= 255), cmap='gray')
-plt.show()
+# plt.imshow(img_thresholded, cmap='gray')
+# plt.show()
 
 # # what channel is best for thresholding out lanes, also Sobel gradients should be considered.
 # img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
@@ -112,23 +149,25 @@ plt.show()
 #     ax[i].imshow(img_hls[..., i])
 # plt.show()
 
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-plt.imshow(img_hsv[..., 2] > 245)
-plt.show()
-fig, ax = plt.subplots(1, 3)
-title_str = ['Hue', 'Saturation', 'Value']
-for i in range(3):
-    ax[i].set_title(title_str[i])
-    ax[i].imshow(img_hsv[..., i])
-plt.show()
+# img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# plt.imshow(img_hsv[..., 2] > 245)
+# plt.show()
+# fig, ax = plt.subplots(1, 3)
+# title_str = ['Hue', 'Saturation', 'Value']
+# for i in range(3):
+#     ax[i].set_title(title_str[i])
+#     ax[i].imshow(img_hsv[..., i])
+# plt.show()
 
 # # apply perspective transform
-# src = np.array([[305, 650], [1000, 650], [685, 450], [595, 450]], np.float32)
-# dst = np.array([[305, img.shape[0]], [1000, img.shape[0]], [1000, 0], [305, 0]], np.float32)
-# trans_mat = cv2.getPerspectiveTransform(src, dst)
-# trans_mat_inverse = cv2.getPerspectiveTransform(dst, src)
-# warped = cv2.warpPerspective(img, trans_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+src = np.array([[305, 650], [1000, 650], [685, 450], [595, 450]], np.float32)
+dst = np.array([[305, img.shape[0]], [1000, img.shape[0]], [1000, 0], [305, 0]], np.float32)
+trans_mat = cv2.getPerspectiveTransform(src, dst)
+trans_mat_inverse = cv2.getPerspectiveTransform(dst, src)
+warped = cv2.warpPerspective(np.uint8(img), trans_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
 
+plt.imshow(warped, cmap='gray')
+plt.show()
 
 # TODO: lane pixel identification
 # TODO: fit curve through the lane pixels
